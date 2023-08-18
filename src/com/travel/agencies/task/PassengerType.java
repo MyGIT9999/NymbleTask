37b11fe190888c47f0dba79795bbd66c5155cf4d
Jay131299 <57975503+Jay131299@users.noreply.github.com>
2023-08-18 18:10:32 +0530

    modified strucutre

--- src/com/travel/agencies/task/PassengerType.java
+++
@@ -1,6 +0,0 @@
-package com.travel.agencies.task;
-
-public interface PassengerType {
-    boolean canSignUpForActivity(double balance, double cost);
-    double calculateCost(double originalCost);
-}
